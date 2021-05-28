bf03ab088a0bb9c247df1885e9480ef5b272446e
honeydev0820 <honeysoft0103@gmail.com>
2021-05-28 16:20:44 +0200

    formatting

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,43 +1,26 @@
-import Image from 'next/image';
 import React from 'react';
-import {
-  Section,
 
-  SectionText,
-  SectionTitle
-} from '../../styles/GlobalComponents';
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection, RightSection } from './HeroStyles';
+import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => {
-  return (
-    <>
-      <Section row>
-        <LeftSection>
-          <SectionTitle main center>
-            Welcome To <br />
-            My Personal Portfolio
-          </SectionTitle>
-          <SectionText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. At
-            auctor urna nunc id cursus metus aliquam eleifend mi.
-          </SectionText>
-          <Button onClick={props.handleClick}>Learn More</Button>
-        </LeftSection>
-        <RightSection>
-          <Image
-            src="/images/profile.jpeg"
-            alt="this is my profile"
-            width="500"
-            height="600"
-            layout="intrinsic"
-          />
-        </RightSection>
-      </Section>
-    </>
-  );
-};
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To <br />
+          My Personal Portfolio
+        </SectionTitle>
+        <SectionText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. At
+          auctor urna nunc id cursus metus aliquam eleifend mi.
+        </SectionText>
+        <Button onClick={props.handleClick}>Learn More</Button>
+      </LeftSection>
+    </Section>
+  </>
+);
 
 export default Hero;
-
